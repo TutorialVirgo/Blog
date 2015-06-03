fdcd6bd9f0f719515c4801bb00f3042ded7d16b1
Juhász Ádám <juhasz.adam@virgo.hu>
2015-06-03 14:07:11 +0200

    Router

--- web/index.php
+++ web/index.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once '..\vendor\autoload.php';
+require_once '../vendor/autoload.php';
 
 use Virgo\Tutorial\Router\SimpleRouter;
 
 $requestURI = explode('/', $_SERVER['REQUEST_URI']);
+var_dump($requestURI);
 $router = new SimpleRouter();
 
 $router->getRoute($requestURI);
