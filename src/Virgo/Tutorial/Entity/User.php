d9647cd7b61f48aa98af37c1ea08e89580ba67af
Juhász Ádám <juhasz.adam@virgo.hu>
2015-06-03 12:35:14 +0200

    User init

---
+++ src/Virgo/Tutorial/Entity/User.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Virgo\Tutorial\Entity;
+
+class User extends AbstractEntity
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @var string
+     */
+    protected $salt;
+
+    /**
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     */
+    public function __construct($name, $email, $password)
+    {
+        parent::__construct();
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    private function generateSalt()
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randstring = '';
+        for ($i = 0; $i < 10; $i++) {
+            $randstring = $characters[rand(0, strlen($characters))];
+        }
+
+        $this->salt = $randstring;
+    }
+
+}
