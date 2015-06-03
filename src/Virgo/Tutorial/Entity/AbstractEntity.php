32eaf9ebf6c4bc8050f4835e47eb16da66051f96
Isty001 <isty001@gmail.com>
2015-06-03 12:33:57 +0200

    valami

--- src/Virgo/Tutorial/Entity/AbstractEntity.php
+++ src/Virgo/Tutorial/Entity/AbstractEntity.php
@@ -4,5 +4,81 @@ namespace Virgo\Tutorial\Entity;
 
 class AbstractEntity
 {
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var \DateTime
+     */
+    protected $createdDate;
+
+    /**
+     * @var \DateTime
+     */
+
+    protected $modifiedDate;
+
+    /**
+     * @var string
+     */
+    protected $status;
+
+
+
+    public function __construct(){
+        $this->createdDate = new \Datetime;
+        $this->modifiedDate = new \Datetime;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedDate()
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getModifiedDate()
+    {
+        return $this->modifiedDate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param \DateTime $modifiedDate
+     */
+    public function setModifiedDate(\DateTime $modifiedDate)
+    {
+        $this->modifiedDate = $modifiedDate;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
 
 }
