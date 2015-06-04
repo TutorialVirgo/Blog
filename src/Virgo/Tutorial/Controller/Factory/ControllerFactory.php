8851c11bf26666b287da0879d89db69e05eea3f4
Isty001 <isty001@gmail.com>
2015-06-04 15:49:16 +0200

    resolver

--- src/Virgo/Tutorial/Controller/Factory/ControllerFactory.php
+++ src/Virgo/Tutorial/Controller/Factory/ControllerFactory.php
@@ -3,7 +3,36 @@
 namespace Virgo\Tutorial\Controller\Factory;
 
 
+use Doctrine\ORM\EntityManager;
+
 class ControllerFactory
 {
+    /**
+     * @var EntityManager $em
+     */
+    private $em;
+
+    /**
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em){
+        $this->em = $em;
+    }
+
+    /**
+     * @param string $className
+     * @return object
+     */
+    public function factory($className){
+
+        $className = 'Virgo\Tutorial\Controller\\' . $className;
+
+
+        if( !class_exists($className)){
+            throw new \InvalidArgumentException;
+        } else {
+            return new $className;
+        }
+    }
 
 }
